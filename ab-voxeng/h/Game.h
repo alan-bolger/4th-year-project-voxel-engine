d274100761faf2573ea855be50b11ef656973c3d
alan-bolger <alan.j.bolger@gmail.com>
2021-04-27 07:06:29 +0100

    Can now remove voxels from neighbouring chunks with mouse click.

--- ab-voxeng/h/Game.h
+++ ab-voxeng/h/Game.h
@@ -30,23 +30,6 @@
 #include "Debug.h"
 #include "Map.h"
 
-class Ray
-{
-public:
-	// TODO: Consider putting this into a struct possibly
-	Ray(const glm::vec3 &orig, const glm::vec3 &dir) : orig(orig), dir(dir)
-	{
-		invdir = 1.0f / dir;
-		sign[0] = (invdir.x < 0);
-		sign[1] = (invdir.y < 0);
-		sign[2] = (invdir.z < 0);
-	}
-
-	glm::vec3 orig, dir; // ray orig and dir 
-	glm::vec3 invdir;
-	int sign[3];
-};
-
 class Game
 {
 public:
@@ -55,14 +38,7 @@ public:
 	void start();
 
 private:
-	// TODO: Move this struct somewhere else
-	struct HitInfo
-	{
-		int m_bi;
-		glm::vec2 m_lambda;
-		glm::vec3 m_center;
-	}
-	m_hitInfo;
+	HitInfo m_hitInfo;
 
 	SDL_Window *m_window;
 	SDL_GLContext m_glContext;
@@ -173,113 +149,16 @@ private:
 	void update(double t_deltaTime);
 	void draw();
 	int getChunkIndex(int x, int y, int z);
+	void updateEntireMap();
 	void initialiseRaytracing();
 	void raytrace();
 	void renderTextureToQuad(GLuint &t_textureID);
 	glm::vec2 intersectCube(glm::vec3 t_origin, glm::vec3 t_direction, glm::vec4 t_cubeCenter);
 	bool checkAllCubesIntersect(glm::vec3 t_origin, glm::vec3 t_direction, HitInfo &t_hitInfo);
-
-	bool intersect(const Ray &r, float &t, int x, int y, int z) const
-	{
-		//glm::vec3 bounds[2];
-		//glm::vec3 f_cubeSize(0.5, 0.5, 0.5);
-		//glm::vec3 f_cubeCenter = glm::vec3(x, y, z);
-
-		//bounds[0] = f_cubeCenter - f_cubeSize;
-		//bounds[1] = f_cubeCenter + f_cubeSize;
-
-		//float tmin, tmax, tymin, tymax, tzmin, tzmax;
-
-		//tmin = (bounds[r.sign[0]].x - r.orig.x) * r.invdir.x;
-		//tmax = (bounds[1 - r.sign[0]].x - r.orig.x) * r.invdir.x;
-		//tymin = (bounds[r.sign[1]].y - r.orig.y) * r.invdir.y;
-		//tymax = (bounds[1 - r.sign[1]].y - r.orig.y) * r.invdir.y;
-
-		//if ((tmin > tymax) || (tymin > tmax))
-		//	return false;
-
-		//if (tymin > tmin)
-		//	tmin = tymin;
-		//if (tymax < tmax)
-		//	tmax = tymax;
-
-		//tzmin = (bounds[r.sign[2]].z - r.orig.z) * r.invdir.z;
-		//tzmax = (bounds[1 - r.sign[2]].z - r.orig.z) * r.invdir.z;
-
-		//if ((tmin > tzmax) || (tzmin > tmax))
-		//	return false;
-
-		//if (tzmin > tmin)
-		//	tmin = tzmin;
-		//if (tzmax < tmax)
-		//	tmax = tzmax;
-
-		//t = tmin;
-
-		//if (t < 0) {
-		//	t = tmax;
-		//	if (t < 0) return false;
-		//}
-
-		//return true;
-	}
+	Indices getChunkXYZ(int x, int y, int z);
 	
 	// This is the function I'm currently using
-	bool intersectAllCubes(glm::vec3 t_origin, glm::vec3 t_direction, int &t_x, int &t_y, int &t_z, glm::vec3 &t_hitPoint)
-	{
-		Ray ray(t_origin, t_direction);
-		float t;
-
-		// Only check neighbouring chunks for intersections
-		glm::vec3 worldPosition = m_camera->getEye();
-
-		for 
-		int chunkCenter = getChunkIndex(worldPosition.x, worldPosition.y, worldPosition.z);
-		int chunkCenter = getChunkIndex(worldPosition.x, worldPosition.y, worldPosition.z);
-		int chunkCenter = getChunkIndex(worldPosition.x, worldPosition.y, worldPosition.z);
-
-		int map_w = MAP_WIDTH / 16;
-		int map_h = MAP_HEIGHT / 16;
-		int map_d = MAP_DEPTH / 16;
-
-		std::vector<Indices> neighbouringChunks = { chunkIndex  };
-
-		for (int i = 0; i < m_voxelPositions.size(); i++)
-		{
-			if (intersect(ray, t, m_voxelPositions[i]))
-			{
-				t_index = i;
-				m_hitPoint = ray.orig + ray.dir * t;
-				return true;
-			}
-		}
-
-		for (int z = 0; z < 256; ++z)
-		{
-			for (int y = 0; y < 256; ++y)
-			{
-				for (int x = 0; x < 256; ++x)
-				{
-					if (m_voxelPositions->at(x, y, z) == false)
-					{
-						break;
-					}
-
-					if (intersect(ray, t, x, y, z))
-					{
-						m_hitPoint = ray.orig + ray.dir * t;
-						t_x = x;
-						t_y = y;
-						t_z = z;
-
-						return true;
-					}
-				}
-			}
-		}
-
-		return false;
-	}
+	bool checkForVoxelIntersections(glm::vec3 t_origin, glm::vec3 t_direction, int& t_x, int& t_y, int& t_z, glm::vec3& t_hitPoint);
 };
 
 #endif // !GAME_H
